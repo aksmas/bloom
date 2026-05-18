eb8dca377510e9c0eaf6ceadaca425d9fa2d343c
Akshay Sunil Masare <akshaymasare@gmail.com>
2026-05-18 19:50:33 +0530

    working Lobby

--- scripts/ui/lobby.gd
+++ scripts/ui/lobby.gd
@@ -1,7 +1,6 @@
-extends Panel
+extends ColorRect
 
 @onready var manager: GameManagerCore = $".."
-
 
 func _process(_delta: float) -> void:
 	if manager.status == CoreConstants.GameState.LOBBY:
@@ -10,5 +9,32 @@ func _process(_delta: float) -> void:
 		visible = false
 
 
-func add_player(name: String) -> void:
-	manager.add_player(PlayerCore.new())
+func show_error(message: String) -> void:
+	$ErrorLabel.text = message
+	$ErrorLabel.show()
+	$ErrorLabel/Timer.start(2)
+
+
+func _update_player_display() -> void:
+	var txt := ""
+	$NewPlayer.text = txt
+	for player in manager.players:
+		txt += ("Player%d \n" % player.ID) + player.NAME + "\n\n"
+	$Players.text = txt
+
+
+func add_player() -> void:
+	var pname: String = $NewPlayer.text
+	if pname.length() < 3:
+		show_error("Player Name should be atleast 3 letters")
+		return
+	var valid := manager.add_player(PlayerCore.new(manager.players.size() + 1, pname))
+	if not valid:
+		show_error("Game is full")
+	_update_player_display()
+
+
+func start_game() -> void:
+	var valid := manager.start()
+	if not valid:
+		show_error("Need atleast 2 Players to Start")
